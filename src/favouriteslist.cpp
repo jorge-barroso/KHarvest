7bd9ce63eed77f1b595ccd54cb32b0e583c85821
jorge-barroso <jorge_barroso_11@hotmail.com>
2023-02-11 05:26:33 +0000

    Fixed favourites QVector initialisation

--- src/favouriteslist.cpp
+++ src/favouriteslist.cpp
@@ -6,24 +6,14 @@
 #include "kharvestconfig.h"
 
 FavouritesList::FavouritesList(QObject *parent)
-        : QObject(parent), mFavourites(KHarvestConfig::favourites_list().size()) {
-    mFavourites.append(new Task{
-            .projectId=123432,
-            .taskId=657621,
-            .clientName="Sample Client",
-            .projectName="Sample Project",
-            .taskName = "Sample Task",
-            .note="Note",
-    });
-
+        : QObject(parent), mFavourites() {
+    mFavourites.reserve(KHarvestConfig::favourites_list().size());
     const QStringList favourites{KHarvestConfig::favourites_list()};
-    std::for_each(favourites.begin(),
-                  favourites.end(),
-                  [this](const QString &taskLine) {
-                      Task *task = new Task{};
-                      taskLine >> *task;
-                      this->favouriteAdded(task);
-                  });
+    std::for_each(favourites.begin(), favourites.end(), [this](const QString &favouriteString) {
+        Task *task = new Task{};
+        favouriteString >> *task;
+        this->favouriteAdded(task);
+    });
 }
 
 FavouritesList::~FavouritesList() {
